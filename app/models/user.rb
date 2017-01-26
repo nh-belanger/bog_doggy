df474774c320aecd24a9fba0ca0ad6c97b7de6fb
nh-belanger <nick.h.belanger@gmail.com>
2017-01-25 22:43:30 -0500

    added devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+  validates :email, presence: true
+
 
   has_many :dogs
 end
